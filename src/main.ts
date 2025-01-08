12cd1a7b3381a3317e23c0ff8d1533ee17878bed
Jean-Noel Drugmand <jean-noel.drugmand@playplay.com>
2025-01-08 10:15:52 +0100

    First commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+import { Application} from "pixi.js"
+
+
+
+const app = new Application({ width: 500, height: 500, background: "#083970"})
+
+document.body.appendChild(app.view)
+
+
