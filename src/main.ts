a1f81d4d316a543d67768c5d5daca4540e7952c4
Jean-Noel Drugmand <jean-noel.drugmand@playplay.com>
2025-01-09 14:20:50 +0100

    creating the sprites for the background

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,35 @@
-import './style.css'
-import { Application} from "pixi.js"
+import { generateSprite } from './generateSprite';
+import './style.css';
+import { Application, Container } from 'pixi.js';
 
+const PRIMARY_COLOR = '#083970';
+const SECONDARY_COLOR = '#6abfb6';
 
+const app = new Application<HTMLCanvasElement>({
+	width: 500,
+	height: 500,
+	background: PRIMARY_COLOR,
+	antialias: true,
+});
 
-const app = new Application({ width: 500, height: 500, background: "#083970"})
+globalThis.__PIXI_APP__ = app;
 
-document.body.appendChild(app.view)
+document.body.appendChild(app.view);
 
+const graphSizeX = 20;
+const graphSizeY = 5;
 
+const gridSize = 5;
+
+const container: Container = new Container();
+
+for (let i = 0; i < gridSize; i++) {
+	for (let j = 0; j < gridSize; j++) {
+		const sprite = generateSprite(graphSizeX, graphSizeY, SECONDARY_COLOR);
+		sprite.x = 0 + i * 100;
+		sprite.y = 0 + j * 100;
+		container.addChild(sprite);
+	}
+}
+
+app.stage.addChild(container);
