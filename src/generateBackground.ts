8d6fa039d61680a9ff3847ea9364ffb30cdba498
Jean-Noel Drugmand <jean-noel.drugmand@playplay.com>
2025-01-10 17:39:22 +0100

    create a generateBackground function

---
+++ src/generateBackground.ts
@@ -0,0 +1,60 @@
+import { Application, Container, Sprite } from 'pixi.js';
+import { generateSprite } from './generateSprite';
+import { getRandom } from './main';
+import seedrandom from 'seedrandom';
+import anime from 'animejs';
+
+export function generateBackground(
+	gridsize: number,
+	graphSizeX: number,
+	graphSizeY: number,
+	color: string,
+	app: Application<HTMLCanvasElement>,
+	randomNumber: seedrandom.PRNG
+) {
+	console.log('random in generateBackground :', randomNumber());
+
+	const backgroundContainer = new Container();
+	const sprites: Sprite[] = [];
+	for (let i = 0; i < gridsize; i++) {
+		for (let j = 0; j < gridsize; j++) {
+			const sprite = generateSprite(graphSizeX, graphSizeY, color);
+			sprite.x =
+				0 +
+				i * (app.screen.width / gridsize) +
+				(app.screen.width / gridsize / 2) * randomNumber();
+			sprite.y =
+				0 +
+				j * (app.screen.height / gridsize) +
+				(app.screen.height / gridsize / 2) * randomNumber();
+
+			sprite.rotation = getRandom(0, Math.PI);
+
+			sprites.push(sprite);
+			backgroundContainer.addChild(sprite);
+		}
+	}
+	const timeline = anime.timeline({
+		loop: true,
+		autoplay: true,
+	});
+
+	timeline.add({
+		targets: sprites,
+		keyframes: [
+			{
+				rotation: (el: Sprite) => [
+					el.rotation,
+					el.rotation + Math.PI / 10,
+				],
+			},
+			{
+				rotation: (el: Sprite) => [
+					el.rotation + Math.PI / 10,
+					el.rotation,
+				],
+			},
+		],
+	});
+	return backgroundContainer;
+}
