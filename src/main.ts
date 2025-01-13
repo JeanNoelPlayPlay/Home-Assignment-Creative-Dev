67c0fd986979b493f4c4d2cc20400218787caaf8
Jean-Noel Drugmand <jean-noel.drugmand@playplay.com>
2025-01-13 22:27:25 +0100

    main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import './style.css';
 import seedrandom from 'seedrandom';
 import anime from 'animejs';
-import { Application, Container, Text, TextStyle } from 'pixi.js';
+import {
+	Application,
+	Container,
+	Sprite,
+	Text,
+	TextStyle,
+	Texture,
+} from 'pixi.js';
 import { generateBackground } from './generateBackground';
 
 const PRIMARY_COLOR = '#083970';
@@ -26,6 +33,24 @@ document.body.appendChild(app.view);
 const randomNumber: seedrandom.PRNG = seedrandom('hello');
 console.log(randomNumber());
 
+const controlContainer = new Container();
+const playBtnTexture = Texture.from('play-solid.svg');
+const stopBtnTexture = Texture.from('stop-solid.svg');
+
+const playBtn = new Sprite(playBtnTexture);
+const stopBtn = new Sprite(stopBtnTexture);
+playBtn.scale.set(0.2);
+playBtn.anchor.set(0.5);
+playBtn.x = app.stage.width / 2 - 50;
+
+stopBtn.scale.set(0.2);
+stopBtn.anchor.set(0.5);
+stopBtn.x = app.stage.width / 2 + 50;
+
+controlContainer.addChild(playBtn, stopBtn);
+controlContainer.x = app.view.width / 2;
+controlContainer.y = app.view.height - 50;
+
 export function getRandom(min: number, max: number) {
 	return randomNumber() * (max - min) + min;
 }
@@ -47,11 +72,9 @@ backgroundContainer.x = app.screen.width / 2 - backgroundContainer.width / 2;
 backgroundContainer.y = app.screen.height / 2 - backgroundContainer.height / 2;
 app.stage.addChild(backgroundContainer);
 
-const TEXT = 'Creative Developer';
+const TEXT = 'Creative Developer\nat PlayPlay';
 const textContainer = new Container();
 const bgTextContainer = new Container();
-let xOffset = 0;
-const yBgOffset = 1;
 const letters: Text[] = [];
 const bgLetters: Text[] = [];
 
@@ -68,18 +91,30 @@ const backgroundTextStyle = new TextStyle({
 	align: 'center',
 });
 
-TEXT.split('').forEach((char) => {
-	const letter: Text = new Text(char, foreGroundTextStyle);
-	const bgLetter: Text = new Text(char, backgroundTextStyle);
-	letter.x = xOffset;
-	bgLetter.x = xOffset;
-	bgLetter.y = yBgOffset;
-	letter.alpha = 0;
-	bgLetter.alpha = 0;
-	xOffset += letter.width;
-	textContainer.addChild(bgLetter, letter);
-	letters.push(bgLetter, letter);
-	// bgLetters.push(bgLetter);
+let yOffset = 0;
+
+TEXT.split('\n').forEach((line) => {
+	let xOffset = 0;
+
+	line.split('').forEach((char) => {
+		const letter: Text = new Text(char, foreGroundTextStyle);
+		const bgLetter: Text = new Text(char, backgroundTextStyle);
+
+		letter.x = xOffset;
+		letter.y = yOffset;
+		letter.alpha = 0;
+
+		bgLetter.x = xOffset;
+		bgLetter.y = yOffset + 1;
+		bgLetter.alpha = 0;
+
+		xOffset += letter.width;
+
+		textContainer.addChild(bgLetter, letter);
+		letters.push(bgLetter, letter);
+		// bgLetters.push(bgLetter);
+	});
+	yOffset += 100;
 });
 
 textContainer.x = (app.renderer.width - textContainer.width) / 2;
@@ -88,41 +123,38 @@ bgTextContainer.x = (app.renderer.width - bgTextContainer.width) / 2;
 bgTextContainer.y = (app.renderer.height - bgTextContainer.height) / 2;
 
 const shuffledLetters = shuffleText(letters);
-
 // const shuffledBgLetters = shuffleText(bgLetters);
-console.log(shuffledLetters);
-const timeline = anime
-	.timeline({
+
+const timeline = anime.timeline({
+	easing: 'easeInOutSine',
+	duration: 400,
+	autoplay: false,
+});
+timeline.add({
+	targets: shuffledLetters,
+	alpha: 1, // Faire apparaître progressivement
+	delay: (el, i) => i * 30, // Décalage progressif
+	duration: 500,
+});
+for (const letter of letters) {
+	timeline.add({
+		targets: letter,
+		y: [
+			{ value: -5, duration: 400 },
+			{ value: 5, duration: 400 },
+		],
+		direction: 'alternate',
+		loop: true,
 		easing: 'easeInOutSine',
-		duration: 4000,
-	})
-	.add({
-		targets: [shuffledLetters],
-		alpha: 1,
-		delay: (el, i) => i * 100,
-		easing: 'easeInOutQuad',
-		duration: 500,
 	});
-// for (const letter of letters) {
-// 	console.log(letter.text, letter.x, letter.y);
-// 	timeline.add(
-// 		{
-// 			targets: letter,
-// 			x: function (el, i) {
-// 				return [el.x - 5, el.x + 5];
-// 			},
-// 			y: function (el, i) {
-// 				return [el.y - 5, el.y + 5];
-// 			},
-// 			// duration: 4000,
-// 			delay: anime.stagger(10),
-// 			easing: 'easeInOutSine',
-// 			loop: true,
-// 			direction: 'alternate',
-// 		},
-// 		'-=500'
-// 	);
-// }
+}
+
+playBtn.eventMode = 'static';
+stopBtn.eventMode = 'static';
+
+playBtn.onclick = timeline.play;
+stopBtn.onclick = timeline.pause;
 
 app.stage.addChild(bgTextContainer);
 app.stage.addChild(textContainer);
+app.stage.addChild(controlContainer);
