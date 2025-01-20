b38d97a8cf828630c5150591ccd38ce4293ce0f2
Jean-Noel Drugmand <jean-noel.drugmand@playplay.com>
2025-01-20 12:21:37 +0100

    generateText

---
+++ src/text/generateText.ts
@@ -0,0 +1,52 @@
+import { Application, BitmapText, Container } from 'pixi.js';
+import { blur, noise } from './textFilters';
+
+export function generateText(
+	text: string,
+	app: Application<HTMLCanvasElement>
+) {
+	const letters: BitmapText[] = [];
+	const foregroundLetters: BitmapText[] = [];
+	const backgroundLetters: BitmapText[] = [];
+	const textContainer = new Container();
+	let yOffset = 0;
+
+	text.split('\n').forEach((line: string) => {
+		let xOffset = 0;
+		const lineContainer = new Container();
+
+		line.split('').forEach((char) => {
+			const letter: BitmapText = new BitmapText(char, {
+				fontName: 'foregroundFont',
+			});
+			const bgLetter: BitmapText = new BitmapText(char, {
+				fontName: 'backgroundFont',
+			});
+
+			letter.x = xOffset;
+			bgLetter.x = xOffset;
+			bgLetter.y = 1;
+
+			xOffset += letter.width;
+
+			lineContainer.addChild(bgLetter, letter);
+			letters.push(bgLetter, letter);
+			foregroundLetters.push(letter);
+			backgroundLetters.push(bgLetter);
+		});
+
+		lineContainer.y = yOffset;
+
+		textContainer.addChild(lineContainer);
+
+		yOffset += lineContainer.height + 10;
+
+		lineContainer.x = app.renderer.width / 2 - lineContainer.width / 2;
+		textContainer.y = app.screen.height / 2 - textContainer.height / 2;
+	});
+	//------------------------FILTERS---------------------------//
+	textContainer.filters = [noise, blur];
+	//----------------------------------------------------------//
+
+	return { textContainer, letters, foregroundLetters, backgroundLetters };
+}
