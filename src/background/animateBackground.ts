7d5144a5ce5573898e495096eb75663424d7c45c
Jean-Noel Drugmand <jean-noel.drugmand@playplay.com>
2025-01-21 14:18:31 +0100

    animateBackground

--- src/background/animateBackground.ts
+++ src/background/animateBackground.ts
@@ -35,9 +35,22 @@ export function animateBackground(
 					el.rotation,
 				],
 			},
+			// {
+			// 	rotation: (el: Graphics) => [
+			// 		el.rotation,
+			// 		el.rotation + Math.PI / 10,
+			// 	],
+			// 	delay: 100,
+			// },
+			// {
+			// 	rotation: (el: Graphics) => [
+			// 		el.rotation + Math.PI / 10,
+			// 		el.rotation,
+			// 	],
+			// },
 		],
 		easing: 'steps(1)',
-		duration: 3000,
+		// duration: 3000,
 		loop: true,
 	});
 	return timeline;
