7d990f907d2fde3016848de5ee593b63cde8d099
Jean-Noel Drugmand <jean-noel.drugmand@playplay.com>
2025-01-27 18:38:36 +0100

    main

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,19 @@
 import './style.css';
 import { Application } from 'pixi.js';
-import { generateBackground } from './background/generateBackground';
+import { generateBackground } from './background/_generateBackground';
 import { randomNumber, shuffleText } from './utils/utils';
 import { animateBackground } from './background/animateBackground';
 import { generateText } from './text/generateText';
 import { PRIMARY_COLOR, SECONDARY_COLOR } from './utils/colors';
 import { animateText } from './text/animateText';
 import anime from 'animejs';
+import { Background } from './background/classes/Background';
 
 const GRAPHSIZEX = 20;
 const GRAPHSIZEY = 3;
 const GRIDSIZE = 5;
 
-const TEXT = 'Creative Developer\nat PlayPlay';
+const TEXT = 'Crea&tive Developer\nat PlayPlay';
 
 const app = new Application<HTMLCanvasElement>({
 	width: 500,
@@ -28,7 +29,15 @@ document.body.appendChild(app.view);
 //------------------------ BACKGROUND -----------------------//
 
 // Create background
-const { backgroundContainer, bgGraph } = generateBackground(
+// const { backgroundContainer, bgGraph } = generateBackground(
+// 	GRIDSIZE,
+// 	GRAPHSIZEX,
+// 	GRAPHSIZEY,
+// 	SECONDARY_COLOR,
+// 	app,
+// 	randomNumber
+// );
+const background = new Background(
 	GRIDSIZE,
 	GRAPHSIZEX,
 	GRAPHSIZEY,
@@ -36,9 +45,10 @@ const { backgroundContainer, bgGraph } = generateBackground(
 	app,
 	randomNumber
 );
-backgroundContainer.x = app.screen.width / 2 - backgroundContainer.width / 2;
-backgroundContainer.y = app.screen.height / 2 - backgroundContainer.height / 2;
-app.stage.addChild(backgroundContainer);
+
+background.x = app.screen.width / 2 - background.width / 2;
+background.y = app.screen.height / 2 - background.height / 2;
+app.stage.addChild(background);
 
 //------------------------ TEXT ---------------------------//
 // create text
@@ -67,7 +77,7 @@ const timelineAnimation = anime.timeline({
 	},
 });
 // Animate background
-animateBackground(bgGraph, timelineAnimation);
+animateBackground(background.bgGraph, timelineAnimation);
 
 // Animate text
 animateText(
@@ -80,6 +90,7 @@ const playBtn: HTMLButtonElement | null = document.querySelector('.playBtn');
 if (playBtn) {
 	playBtn.onclick = timelineAnimation.play;
 }
+
 const stopBtn: HTMLButtonElement | null = document.querySelector('.stopBtn');
 if (stopBtn) {
 	stopBtn.onclick = timelineAnimation.pause;
